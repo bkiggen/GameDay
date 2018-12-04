c2ad08978bca07b87e41e800d466698ee0bea5e8
Ben Kiggen and Joe Friesen <joe@>
2018-12-04 11:49:56 -0800

    add calendar ui

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import config from './../../.env';
 import firebase from 'firebase/app';
 import * as firebaseui from 'firebaseui';
+import CreateCalendarEntry from './CreateCalendarEntry';
 
 class Auth extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      calendarInfo: null
-    }
+      calendarInfo: null,
+      loggedIn: false,
+      currentUser: null
+    };
   }
 
   componentDidMount() {
@@ -29,78 +32,91 @@ class Auth extends React.Component{
     ui.start('#firebaseui-auth-container', uiConfig);
 
     firebase.auth().onAuthStateChanged(user => {
-      console.log("onAuthStateChanged fired. user:", user);
       if (user) {
-        const script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src = "https://apis.google.com/js/api.js";
-        console.log("gapi.client: ", script.src);
-        script.onload = function(e) {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = 'https://apis.google.com/js/api.js';
+        script.onload = e => {
           gapi.load('client', {
-            callback: function() {
+            callback: () => {
               gapi.client.init({
                 apiKey: config.apiKey,
                 clientId: config.clientId,
                 discoveryDocs: config.discoveryDocs,
-                scope: config.scopes.join(" ")
+                scope: config.scopes.join(' ')
               })
-              .then(() => {
-                if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
-                  startApp(user);
-                } else {
-                  firebase.auth().signOut();
-                }
-              });
+                .then(() => {
+                  if (gapi.auth2.getAuthInstance().isSignedIn.get()) {
+                    startApp(user);
+                    this.setState({
+                      loggedIn: true
+                    });
+                  } else {
+                    firebase.auth().signOut();
+                  }
+                });
             }
-          })
+          });
         };
-        document.getElementsByTagName("head")[0].appendChild(script);
+        document.getElementsByTagName('head')[0].appendChild(script);
       }
     });
 
     const startApp = (user) => {
-      console.log("startApp fired");
-      console.log(firebase.auth().currentUser);
-      firebase.auth().currentUser.getIdToken()
-      .then(() => {
-        return gapi.client.calendar.events.list({
-          calendarId: "primary",
-          timeMin: new Date().toISOString(),
-          showDeleted: false,
-          singleEvents: true,
-          maxResults: 40,
-          orderBy: "startTime"
-        })
-      })
-      .then(response => {
-        console.log("firebase.auth().currentUser.getToken promise fulfilled. response: ", response);
-        this.setState({
-          calendarInfo: response.result.items
-        });
+      this.setState({
+        currentUser: user
       });
-    }
+      console.log(this.state.currentUser);
+      firebase.auth().currentUser.getIdToken()
+        .then(() => {
+          return gapi.client.calendar.events.list({
+            calendarId: 'primary',
+            timeMin: new Date().toISOString(),
+            showDeleted: false,
+            singleEvents: true,
+            maxResults: 40,
+            orderBy: 'startTime'
+          });
+        })
+        .then(response => {
+          this.setState({
+            calendarInfo: response.result.items
+          });
+        });
+    };
 
   }
 
   render() {
-    console.log(this.state.calendarInfo);
     let firstCalendarEntry;
+    let renderNewCalendarEntryComponent;
     if (!this.state.calendarInfo) {
-      console.log('if...');
-      firstCalendarEntry = "Getting Calendar Info...";
+      firstCalendarEntry = [{
+        summary: 'Getting calendar info...',
+        id: 0
+      }];
     } else {
-      console.log('else...');
-      firstCalendarEntry = this.state.calendarInfo[0].summary;
+      firstCalendarEntry = this.state.calendarInfo;
     }
+    if (this.state.loggedIn) {
+      renderNewCalendarEntryComponent = <CreateCalendarEntry
+        user={this.state.currentUser} />;
+    } else {
+      renderNewCalendarEntryComponent = 'Log in to create a new calendar entry';
+    }
+    console.log(this.state);
     return (
-      <div key={this.state.calendarInfo}>
+      <div>
         <h1>Firebase Auth Quickstart</h1>
         <div id="firebaseui-auth-container"></div>
         <div id="calendar-info">
           <ul>
-            <li>{firstCalendarEntry}</li>
+            {firstCalendarEntry.map((key) => {
+              return <li key={key.id}>{key.summary}</li>;
+            })}
           </ul>
         </div>
+        <div>{renderNewCalendarEntryComponent}</div>
       </div>
     );
   }
---
+++ src/components/CreateCalendarEntry.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function CreateCalendarEntry() {
+  return (
+    <div>
+      <p>What up?</p>
+    </div>
+  );
+}
